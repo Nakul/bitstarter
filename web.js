fd33dfebf6b37d67f05b8f458fb66f0282d4c0f5
Nakul <nakulthemaster@gmail.com>
2013-07-12 00:35:23 +0530

    Added buffer code

--- web.js
+++ web.js
@@ -1,17 +1,15 @@
 var fs = require("fs");
 
-var mybuffer = new Buffer(64);
+//var mybuffer = new Buffer(64);
 
-//mybuffer=fs.ReadFileSync('/home/nakul/bitstarter/index.html');
-
-//var data = mybuffer.toString(0,27);
+var data = fs.ReadFileSync("/home/nakul/bitstarter/index.html");
 
 var express = require('express');
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-		response.send('Nakul');
+		response.send(data);
 });
 
 var port = process.env.PORT || 5000;
