16a5c3ae6bd1bd04bcbe1bc603bff9904734063f
Nakul <nakulthemaster@gmail.com>
2013-07-12 00:39:01 +0530

    Added buffer code

--- web.js
+++ web.js
@@ -2,7 +2,7 @@ var fs = require("fs");
 
 //var mybuffer = new Buffer(64);
 
-var data = fs.ReadFileSync("/home/nakul/bitstarter/index.html");
+var data = fs.readFileSync("/home/nakul/bitstarter/index.html");
 
 var express = require('express');
 
